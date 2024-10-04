9f034abeaa4d1b4d73923542d5d25390312ef168
kkn1125 <xpfjrnseks1@naver.com>
2024-10-04 14:49:15 +0900

    🛠️ fix: 리빌드

--- src/common/variables.d.ts
+++ src/common/variables.d.ts
@@ -2,3 +2,4 @@ export declare const DB_HOST: string;
 export declare const DB_USER: string;
 export declare const DB_PW: string;
 export declare const OUTPUT_DIR: string;
+export declare const VERSION: string;
